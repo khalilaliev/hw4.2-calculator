c6ed7363aec712d2d984c522649c5bc9b866ce85
Khalil <khalilaliev0@gmail.com>
2025-12-05 12:22:01 +0100

    add logic to caculator

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Enter first number: ");
+int firstMultiplier = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int secondMultiplier = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter an operator (+, -, *, /): ");
+string? operation = Console.ReadLine();
+
+int result = 0;
+bool success = true;
+
+if (operation == "+")
+{
+    result = firstMultiplier + secondMultiplier;
+}
+else if (operation == "-")
+{
+    result = firstMultiplier - secondMultiplier;
+}
+else if (operation == "*")
+{
+    result = firstMultiplier * secondMultiplier;
+}
+else if (operation == "/")
+{
+    if (secondMultiplier == 0)
+    {
+        Console.WriteLine("You can't divide by 0.");
+        success = false;
+    }
+    else
+    {
+        result = firstMultiplier / secondMultiplier;
+    }
+}
+else
+{
+    Console.WriteLine("Unknown operator.");
+    success = false;
+}
+
+if (success)
+{
+    Console.WriteLine($"Your result is {result}");
+}
