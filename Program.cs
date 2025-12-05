4d269bd894d8a935f61ae5c252ee47d151adeb87
Khalil <khalilaliev0@gmail.com>
2025-12-05 13:33:01 +0100

    remade with swicth case

--- Program.cs
+++ Program.cs
@@ -1,46 +1,42 @@
 ﻿Console.Write("Enter first number: ");
-int firstMultiplier = Convert.ToInt32(Console.ReadLine());
+double firstMultiplier = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter second number: ");
-int secondMultiplier = Convert.ToInt32(Console.ReadLine());
+double secondMultiplier = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter an operator (+, -, *, /): ");
 string? operation = Console.ReadLine();
 
-int result = 0;
+double result = 0;
 bool success = true;
 
-if (operation == "+")
+switch (operation)
 {
-    result = firstMultiplier + secondMultiplier;
-}
-else if (operation == "-")
-{
-    result = firstMultiplier - secondMultiplier;
-}
-else if (operation == "*")
-{
-    result = firstMultiplier * secondMultiplier;
-}
-else if (operation == "/")
-{
-    if (secondMultiplier == 0)
-    {
-        Console.WriteLine("You can't divide by 0.");
-        success = false;
-    }
-    else
-    {
+    case "+":
+        result = firstMultiplier + secondMultiplier;
+        break;
+    case "-":
+        result = firstMultiplier - secondMultiplier;
+        break;
+    case "*":
+        result = firstMultiplier * secondMultiplier;
+        break;
+    case "/":
+        if (secondMultiplier == 0)
+        {
+            Console.WriteLine("Cant devide by 0");
+        }
         result = firstMultiplier / secondMultiplier;
-    }
-}
-else
-{
-    Console.WriteLine("Unknown operator.");
-    success = false;
+        break;
+    default:
+        Console.WriteLine("Unknown operator");
+        success = false;
+        break;
 }
 
 if (success)
 {
+
     Console.WriteLine($"Your result is {result}");
 }
+
